bd26c98201bfeb5da5d4a8a63a4d68373e671f33
Sam Bryant <bryantsj@gmail.com>
2020-01-28 22:27:04 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,13 +1,8 @@
 # Define display_board that accepts a board and prints
-# out the current state.
 def display_board(board)
-  pipe = "|"
-  lines = "-----------"
-
-  puts "#{board[0]} #{pipe} #{board[1]} #{pipe} #{board[2]}"
-  puts "#{lines}"
-  puts "#{board[3]} #{pipe} #{board[4]} #{pipe} #{board[5]}"
-  puts "#{lines}"
-  puts "#{board[6]} #{pipe} #{board[7]} #{pipe} #{board[8]}"
-end
-board = [" ", " ", " ", " ", "X", " ", " ", " ", " "]
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+end 
