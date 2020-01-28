2515755f9aebc524bd6ffe15e1186bc1d5ae917e
Sam Bryant <bryantsj@gmail.com>
2020-01-28 22:38:36 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,12 +1,13 @@
 # Define display_board that accepts a board and prints
 def display_board(board)
-  puts " #{board[0]} |   |   "
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
-  puts "   | #{board[4]} |   "
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
   puts "-----------"
-  puts "   |   |   "
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
   board = [" "," "," "," "," "," "," "," "," "]
 board = [" ", " ", " ", " ", "X", " ", " ", " ", " "]
 board = ["O", " ", " ", " ", "X", " ", " ", " ", " "]
+board = ["X", "X", "X", " ", " ", " ", " ", " ", " "]
